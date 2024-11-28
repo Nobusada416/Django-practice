cf2f5064de16d2e7651f4c24d2063b59180b8583
Nobusada416 <nobusada.okamoto@gmail.com>
2024-11-29 05:55:51 +0900

    add: loggingモジュールの利用

---
+++ 14_LoggingSample/01_logging_sample.py
@@ -0,0 +1,25 @@
+# logging
+
+import logging
+
+logging.basicConfig(
+    level=logging.WARNING,
+    filename='14_LoggingSample/sample.log',
+    filemode='w',
+    format='%(asctime)s-%(process)s-%(levelname)s-%(message)s',
+)
+logging.debug('debug.log')
+logging.info('info.log')
+logging.warning('warning.log')
+logging.error('error.log')
+logging.critical('critical.log')
+
+user = 'Taro'
+logging.error(f'user = {user} raised error')
+
+a = 10
+b = 0
+try:
+    c = a / b
+except Exception as e:
+    logging.error(e, exc_info=True)
